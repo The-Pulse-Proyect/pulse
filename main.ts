a099561722098e0ccbca0521f4b4b584af335a81
Nightmare <fajardoariel435@gmail.com>
2025-08-01 09:08:15 -0400

    init

---
+++ main.ts
@@ -0,0 +1,65 @@
+import { app, BrowserWindow, ipcMain } from 'electron';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import electronSquirrelStartup from 'electron-squirrel-startup';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+
+if (electronSquirrelStartup) {
+  app.quit();
+}
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  // Load the app.
+  console.log(process.env.VITE_DEV_SERVER_URL)
+  if (true) {
+    mainWindow.loadURL('http://localhost:5173');
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
+  } else {
+    mainWindow.loadFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
+  }
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
+
+// Example IPC handler
+ipcMain.handle('get-app-version', () => {
+  return app.getVersion();
+});
